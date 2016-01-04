f17a9774d7edadd10dd1cc126e6e64878aa0ebdd
Yoan Blanc <yoan@dosimple.ch>
2016-01-04 18:03:10 +0100

    Slide 14 (suite): test du contrôlleur
    Un peu de TDD pour le coût. Nous nous assurons tout simplement que la
    page d'accueil ne produit pas d'erreur et affiche une liste de produits.
    
        $ rake test
    
        ....E
    
        5 runs, 9 assertions, 0 failures, 1 errors, 0 skips

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "smoke test on index" do
+    get :index
+
+    assert_response :success
+    assert_not_nil assigns(:products)
+  end
 end
