82299752051086afe992694b3ab05c89e95104ad
Yoan Blanc <yoan@dosimple.ch>
2016-01-04 17:56:38 +0100

    Slide 13: validations
    Notre produit est fun mais manque de logique, tout peut être vide.
    
    La logique s'ajoute dans les modèles et pour la valider des tests
    unitaires sont de bon ton.
    
        $ rake test
    
        ....
    
        4 runs, 9 assertions, 0 failures, 0 errors, 0 skips
    
    Sans faire du TDD jusqu'au boutiste, avoir des tests permet de s'assurer
    qu'on n'insère pas de régression dans le code.
    
    Quand un bogue est trouvé, c'est une excellente pratique de le
    reproduire via un test, **puis** de le corriger. Il n'est pas nécessaire
    que ça soit fait pas la même personne.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ActiveRecord::Base
+  validates :title, presence: true
+  validates :price, numericality: { greater_than: 0 }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "title is required" do
+    product = Product.new(title: nil)
+
+    assert_not product.save
+    assert_includes product.errors.to_h, :title
+  end
+
+  test "price is required" do
+    product = Product.new(price: nil)
+
+    assert_not product.save
+    assert_includes product.errors.to_h, :price
+  end
+
+  test "description is not required" do
+    product = Product.new(title: "Dummy", price: 10)
+
+    assert product.save
+  end
+
+  test "price must be positive" do
+    product = Product.new(title: "Dummy", price: 0)
+    assert_not product.save
+
+    product.price = -1
+    assert_not product.save
+  end
 end
