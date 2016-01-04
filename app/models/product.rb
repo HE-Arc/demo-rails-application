54db5626470b857483333ab131d4667a69bd2d81
Yoan Blanc <yoan@dosimple.ch>
2016-01-04 18:41:04 +0100

    Slide 21: produit avec image
    On ajoute les informations sur notre image puis pouvons nous en servir
    depuis l'interface d'administration car depuis la console c'est un peu
    plus compliqué.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,9 @@ class Product < ActiveRecord::Base
 
   validates :title, presence: true
   validates :price, numericality: { greater_than: 0 }
+
+  has_attached_file :image, styles: { medium: '500x500', thumb: '250x250' },
+                            default_url: '/images/:style/missing.png'
+
+  validates_attachment_content_type :image, content_type: /^image\/.*$/
 end
