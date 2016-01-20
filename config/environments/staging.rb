11e67d395a2db54de01b78e53e212aa99777c94d
Yoan Blanc <yoan@dosimple.ch>
2016-01-20 22:46:04 +0100

    Ajout d'un environnement de staging.
    Utile pour avoir le developpement sur srvz-webapp plutôt que la
    production. Ainsi les deux configurations de la base de données sont
    séparées entre développment local (+ test) et staging/production.
    
    Puma était manquant du Gemfile.

---
+++ config/environments/staging.rb
@@ -0,0 +1,8 @@
+# Based on development defaults
+
+require Rails.root.join("config/environments/development")
+
+Rails.application.configure do
+  # Enable anyone to see the error messages.
+  config.web_console.whitelisted_ips = "157.26.66.0/24"
+end
