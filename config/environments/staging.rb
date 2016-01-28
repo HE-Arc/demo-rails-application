03f1a91807963fdf1b28c7a0cc57ac5ea9d9d6f4
Yoan Blanc <yoan@dosimple.ch>
2016-01-28 10:51:51 +0100

    Bugfix

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,5 +4,5 @@ require Rails.root.join("config/environments/development")
 
 Rails.application.configure do
   # Enable anyone to see the error messages.
-  config.web_console.whitelisted_ips = "157.26.66.0/24"
+  #config.web_console.whitelisted_ips = "157.26.66.0/24"
 end
