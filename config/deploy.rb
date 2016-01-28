ca06c2ead6d62753aa4eb13d185a40421feab362
Yoan Blanc <yoan@dosimple.ch>
2016-01-28 10:51:36 +0100

    Configuration of Capistrano
    Problème: le mot de passe de la base de données doit être mis dans les
    variables données pour que migrate fonctionne.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'app'
+set :repo_url, 'git@github.com:HE-Arc/demo-rails-application.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -26,10 +26,16 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, {
+  "POSTGRES_USERNAME": "admin",
+  "POSTGRES_PASSWORD": "*********",  # FIXME
+  "GEM_HOME": "/var/www/.gem/ruby/2.2.0",
+  "PATH": "/var/www/.gem/ruby/2.2.0/bin:$PATH"
+}
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
@@ -39,9 +45,15 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      #within release_path do
+      #  execute :rake, 'cache:clear'
+      #end
+    end
+  end
+
+  after :finished, :restart_puma do
+    on roles(:web) do
+      execute :sudo, 'sv restart puma'
     end
   end
 
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-
+server 'srvz-webapp.he-arc.ch', user: 'yoan', roles: %w{app db web}
 
 
 # role-based syntax
@@ -59,3 +59,7 @@
 #     auth_methods: %w(publickey password)
 #     # password: 'please use keys'
 #   }
+
+set :ssh_options, {
+  port: 2020
+}
